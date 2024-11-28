5400001ff8fda50c939b72cf4790c7066717298d
Mizu <psdk55052004@gmail.com>
2024-11-28 14:32:02 +0800

    新增 元件: 頁面標題元件

---
+++ components/pageTitle.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+    default: "",
+  },
+  link: {
+    type: String,
+    required: true,
+    default: "",
+  },
+});
+
+const { title, link } = props;
+</script>
+
+<template>
+  <h1 class="mb-10">
+    <a :href="link" class="text-2xl text-sky-600" target="_blank">
+      <!-- 官方核心概念示範 -->
+      {{ title }}
+    </a>
+  </h1>
+</template>
+
+<style lang="scss" scope></style>
