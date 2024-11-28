6b82f0207a48babcaa22a871e9719914e77c4f59
Mizu <psdk55052004@gmail.com>
2024-11-28 14:53:28 +0800

    新增 元件: 練習區塊的元件

---
+++ components/sectionContent.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+    default: "",
+  },
+});
+
+const { title } = props;
+</script>
+
+<template>
+  <section>
+    <h2 class="text-xl mb-3">{{ title }}</h2>
+    <div class="ps-5">
+      <slot name="content" />
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scope></style>
