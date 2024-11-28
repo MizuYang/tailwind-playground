ecfad3dcee98bb12da4fc2e1d292d75995340b3d
Mizu <psdk55052004@gmail.com>
2024-11-28 10:17:27 +0800

    安裝 tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
+  modules: ['@nuxtjs/tailwindcss'],
+
   // 共用樣式
   css: ['@/style/all.scss']
 })
