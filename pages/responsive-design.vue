f99ed46d7dadc4d5eec7daec1e71648c418e57af
mizu <psdk55052004@gmail.com>
2024-12-03 00:44:05 +0800

    新增 卡片響應式

--- pages/responsive-design.vue
+++ pages/responsive-design.vue
@@ -12,9 +12,29 @@ definePageMeta({
     />
 
     <main class="pb-10">
-      <SectionContent title="xxxxx">
+      <SectionContent title="響應式卡片">
         <template #content>
-            efwfe
+          <div
+            class="w-1/2 items-center rounded-xl ring-1 ring-violet-600 lg:flex lg:rounded-l-xl"
+          >
+            <div
+              class="overflow-hidden rounded-t-xl object-cover lg:shrink-0 lg:rounded-none lg:rounded-l-xl"
+            >
+              <img
+                src="http://placehold.it/100x100/000/fff/?text=xDDDD"
+                class="block h-full w-full object-cover lg:h-auto lg:w-auto"
+              />
+            </div>
+            <div class="lg:shrink-0">
+              <div class="w-1/2 px-5 py-3">
+                <h2>標題</h2>
+                <p class="overflow-wrap break-words">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea
+                  quae commodi repellat, minima maxime labore qui, optio,
+                </p>
+              </div>
+            </div>
+          </div>
         </template>
       </SectionContent>
     </main>
