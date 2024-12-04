eccd175ab6bfbfdfae1022dd2403c05be0b45856
Mizu <psdk55052004@gmail.com>
2024-12-04 08:47:02 +0800

    新增 透過 @apply 提取整個類名

--- pages/functions-and-directives.vue
+++ pages/functions-and-directives.vue
@@ -13,6 +13,15 @@
           <button type="button" class="btn-primary test">按鈕</button>
         </template>
       </SectionContent>
+
+      <SectionContent title="透過 @apply 提取整個類名">
+        <template #content>
+          <div class="card">
+            <h2>Card</h2>
+            <p>Card content</p>
+          </div>
+        </template>
+      </SectionContent>
     </main>
   </div>
 </template>
@@ -28,5 +37,29 @@
 /** 測試 !important 可覆蓋此樣式 */
 .test {
   background-color: pink;
+}
+
+.card-content {
+  h2 {
+    font-size: 20px;
+    font-weight: 700;
+    margin-bottom: 5px;
+  }
+  p {
+    font-size: 16px;
+    font-weight: 500;
+  }
+}
+.card-theme-dark {
+  background-color: #000;
+  color: grey;
+}
+.card-spacing {
+  padding: 20px 30px;
+}
+@layer components {
+  .card {
+    @apply card-theme-dark card-content card-spacing;
+  }
 }
 </style>
