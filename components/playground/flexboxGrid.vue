aca49ecbf0eb0d21a849a25dc579ad93949fef2e
Mizu <psdk55052004@gmail.com>
2024-12-05 09:47:12 +0800

    新增 flex: basis

--- components/playground/flexboxGrid.vue
+++ components/playground/flexboxGrid.vue
@@ -2,7 +2,21 @@
 
 <template>
   <div>
-    <SectionContent title="Grid">
+    <SectionContent title="Flex">
+      <template #content>
+        <div>
+          <h3 class="text-red-700 mb-3">flex-basis</h3>
+          <ul class="flex gap-3">
+            <li class="box basis-auto">basis-auto</li>
+            <li class="box basis-[17.1234%]">basis-[17.1234%]</li>
+            <li class="box shrink-0 basis-32">shrink-0 basis-20 (128px)</li>
+            <li class="box basis-1/2">basis-1/2</li>
+          </ul>
+        </div>
+      </template>
+    </SectionContent>
+
+    <!-- <SectionContent title="Grid">
       <template #content>
         <h3 class="text-red-700 mb-3">
           *:w-30 grid grid-cols-3 gap-6 *:h-10 *:bg-sky-300
@@ -16,8 +30,16 @@
           <li></li>
         </ul>
       </template>
-    </SectionContent>
+    </SectionContent> -->
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@tailwind components;
+
+@layer components {
+  .box {
+    @apply bg-sky-300 w-10 h-10 inline-block;
+  }
+}
+</style>
