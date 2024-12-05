28f0e79c4be3cd5871bc63fea585418e1e1cb327
mizu <psdk55052004@gmail.com>
2024-12-06 00:22:19 +0800

    新增 grid: grid.vue

--- pages/playground/grid.vue
+++ pages/playground/grid.vue
@@ -1,11 +1,35 @@
 <script setup>
+definePageMeta({
+  title: "Grid",
+});
 
 </script>
 
 <template>
   <div>
-    ged
+    <SectionContent title="Grid">
+      <template #content>
+        <div class="mb-3">
+          <h3 class="mb-3 text-red-700">
+            grid-cols-*
+          </h3>
+          <div class="grid grid-cols-4 gap-3">
+            <template v-for="i in 8" :key="i">
+              <div class="box">{{ i }}</div>
+            </template>
+          </div>
+        </div>
+      </template>
+    </SectionContent>
   </div>
 </template>
 
-<style lang='scss' scoped></style>
+<style lang="scss" scoped>
+@tailwind components;
+
+@layer components {
+  .box {
+    @apply inline-block bg-sky-300;
+  }
+}
+</style>
