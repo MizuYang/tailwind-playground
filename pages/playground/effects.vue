f7136fa0bba5c0f3fcc80b44c360b379dfa30225
mizu <psdk55052004@gmail.com>
2024-12-12 23:55:15 +0800

    新增 混入混合模式: mix-blend-multiply

--- pages/playground/effects.vue
+++ pages/playground/effects.vue
@@ -5,7 +5,7 @@
     <SectionContent title="Effects">
       <template #content>
         <div class="mb-3">
-          <h3>shadow</h3>
+          <h3 class="mb-3 text-red-700">shadow</h3>
 
           <div
             class="my-5 size-20 bg-sky-400/20 shadow-lg shadow-sky-500/40"
@@ -15,6 +15,22 @@
             class="my-5 inline-block size-20 bg-violet-400/20 text-center leading-[80px] shadow-lg shadow-violet-500/40"
             >按鈕</a
           >
+        </div>
+
+        <div class="mb-3">
+          <h3 class="mb-3 text-red-700">混入混合模式</h3>
+
+          <div>
+            <h4>mix-blend-multiply</h4>
+            <div class="flex">
+              <div
+                class="my-3 size-20 rounded-full bg-sky-300 mix-blend-multiply"
+              ></div>
+              <div
+                class="my-3 size-20 -translate-x-6 rounded-full bg-red-300 mix-blend-multiply"
+              ></div>
+            </div>
+          </div>
         </div>
       </template>
     </SectionContent>
