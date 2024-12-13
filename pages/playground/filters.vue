3d928cafa6158432d7878ff6df1584f29450a94d
Mizu <psdk55052004@gmail.com>
2024-12-13 15:27:03 +0800

    新增 filters: brightness-* 亮度

--- pages/playground/filters.vue
+++ pages/playground/filters.vue
@@ -5,13 +5,24 @@
     <SectionContent title="Effects">
       <template #content>
         <div class="mb-3">
-          <h3 class="mb-3 text-red-700">blur</h3>
+          <h3 class="mb-3 text-red-700">blur 模糊</h3>
 
           <div class="flex gap-x-8">
             <img src='https://picsum.photos/100/100' class="blur-sm" />
             <img src='https://picsum.photos/100/100' class="blur-md" />
             <img src='https://picsum.photos/100/100' class="blur-lg" />
             <img src='https://picsum.photos/100/100' class="blur-xl" />
+          </div>
+        </div>
+
+        <div class="mb-3">
+          <h3 class="mb-3 text-red-700">brightness-* 亮度</h3>
+
+          <div class="flex gap-x-8">
+            <img src='https://picsum.photos/100/100' class="brightness-50" />
+            <img src='https://picsum.photos/100/100' class="brightness-100" />
+            <img src='https://picsum.photos/100/100' class="brightness-125" />
+            <img src='https://picsum.photos/100/100' class="brightness-150" />
           </div>
         </div>
       </template>
