68e32974e07296ea618973a2169e52815f268f4a
mizu <psdk55052004@gmail.com>
2024-12-17 00:17:04 +0800

    新增 table: caption-top, caption-bottom

--- pages/playground/table.vue
+++ pages/playground/table.vue
@@ -6,7 +6,7 @@
       <template #content>
         <div class="mb-3">
           <h3 class="mb-3 text-red-700">table</h3>
-          <table class="border border-solid border-gray-400 border-separate">
+          <table class="border-separate border border-solid border-gray-400">
             <thead>
               <tr>
                 <th class="border border-solid border-gray-400 bg-gray-200 p-4">
@@ -37,7 +37,9 @@
 
         <div class="mb-3">
           <h3 class="mb-3 text-red-700">border-spacing-*</h3>
-          <table class="border border-solid border-spacing-1 border-gray-400 border-separate">
+          <table
+            class="border-separate border-spacing-1 border border-solid border-gray-400"
+          >
             <thead>
               <tr>
                 <th class="border border-solid border-gray-400 bg-gray-200 p-4">
@@ -64,6 +66,113 @@
               </tr>
             </tbody>
           </table>
+        </div>
+
+        <div class="mb-3">
+          <h3 class="mb-3 text-red-700">caption-top, caption-bottom</h3>
+          <div class="flex">
+            <table
+              class="caption-top border-separate border-spacing-1 border border-solid border-gray-400 me-10"
+            >
+              <caption>
+                我是 caption
+              </caption>
+              <thead>
+                <tr>
+                  <th
+                    class="border border-solid border-gray-400 bg-gray-200 p-4"
+                  >
+                    標題 1
+                  </th>
+                  <th
+                    class="border border-solid border-gray-400 bg-gray-200 p-4"
+                  >
+                    標題 2
+                  </th>
+                  <th
+                    class="border border-solid border-gray-400 bg-gray-200 p-4"
+                  >
+                    標題 3
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td class="border border-solid border-gray-400 p-4">
+                    數據 1
+                  </td>
+                  <td class="border border-solid border-gray-400 p-4">
+                    數據 2
+                  </td>
+                  <td class="border border-solid border-gray-400 p-4">
+                    數據 3
+                  </td>
+                </tr>
+                <tr>
+                  <td class="border border-solid border-gray-400 p-4">
+                    數據 4
+                  </td>
+                  <td class="border border-solid border-gray-400 p-4">
+                    數據 5
+                  </td>
+                  <td class="border border-solid border-gray-400 p-4">
+                    數據 6
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <table
+              class="caption-bottom border-separate border-spacing-1 border border-solid border-gray-400"
+            >
+              <caption>
+                我是 caption
+              </caption>
+
+              <thead>
+                <tr>
+                  <th
+                    class="border border-solid border-gray-400 bg-gray-200 p-4"
+                  >
+                    標題 1
+                  </th>
+                  <th
+                    class="border border-solid border-gray-400 bg-gray-200 p-4"
+                  >
+                    標題 2
+                  </th>
+                  <th
+                    class="border border-solid border-gray-400 bg-gray-200 p-4"
+                  >
+                    標題 3
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td class="border border-solid border-gray-400 p-4">
+                    數據 1
+                  </td>
+                  <td class="border border-solid border-gray-400 p-4">
+                    數據 2
+                  </td>
+                  <td class="border border-solid border-gray-400 p-4">
+                    數據 3
+                  </td>
+                </tr>
+                <tr>
+                  <td class="border border-solid border-gray-400 p-4">
+                    數據 4
+                  </td>
+                  <td class="border border-solid border-gray-400 p-4">
+                    數據 5
+                  </td>
+                  <td class="border border-solid border-gray-400 p-4">
+                    數據 6
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </template>
     </SectionContent>
