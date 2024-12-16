9060ba7cabc0c60cf96aa3c55601d75d9cfcbd8d
mizu <psdk55052004@gmail.com>
2024-12-17 00:09:52 +0800

    新增 table: border-spacing-*

--- pages/playground/table.vue
+++ pages/playground/table.vue
@@ -2,11 +2,42 @@
 
 <template>
   <div>
-    <SectionContent title="Effects">
+    <SectionContent title="Table">
       <template #content>
         <div class="mb-3">
-          <h3 class="mb-3 text-red-700">shadow</h3>
-          <table class="border border-solid border-gray-400">
+          <h3 class="mb-3 text-red-700">table</h3>
+          <table class="border border-solid border-gray-400 border-separate">
+            <thead>
+              <tr>
+                <th class="border border-solid border-gray-400 bg-gray-200 p-4">
+                  標題 1
+                </th>
+                <th class="border border-solid border-gray-400 bg-gray-200 p-4">
+                  標題 2
+                </th>
+                <th class="border border-solid border-gray-400 bg-gray-200 p-4">
+                  標題 3
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td class="border border-solid border-gray-400 p-4">數據 1</td>
+                <td class="border border-solid border-gray-400 p-4">數據 2</td>
+                <td class="border border-solid border-gray-400 p-4">數據 3</td>
+              </tr>
+              <tr>
+                <td class="border border-solid border-gray-400 p-4">數據 4</td>
+                <td class="border border-solid border-gray-400 p-4">數據 5</td>
+                <td class="border border-solid border-gray-400 p-4">數據 6</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div class="mb-3">
+          <h3 class="mb-3 text-red-700">border-spacing-*</h3>
+          <table class="border border-solid border-spacing-1 border-gray-400 border-separate">
             <thead>
               <tr>
                 <th class="border border-solid border-gray-400 bg-gray-200 p-4">
