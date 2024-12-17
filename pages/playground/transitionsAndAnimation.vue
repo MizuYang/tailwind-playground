086b2d66024c647f1ec7761de3dc5552478f2e56
mizu <psdk55052004@gmail.com>
2024-12-17 23:31:32 +0800

    新增 transition & animation: transition-opacity

--- pages/playground/transitionsAndAnimation.vue
+++ pages/playground/transitionsAndAnimation.vue
@@ -10,6 +10,13 @@
             class="size-10 bg-red-400 transition-colors hover:bg-sky-400"
           ></div>
         </div>
+        
+        <div class="mb-3">
+          <h3 class="mb-3 text-red-700">transition-colors</h3>
+          <div
+            class="size-10 bg-red-400 hover:opacity-70 transition-opacity"
+          ></div>
+        </div>
       </template>
     </SectionContent>
   </div>
