d3a1ab6c98495c47aecb1c17f97752461afc1104
mizu <psdk55052004@gmail.com>
2024-12-17 23:36:48 +0800

    新增 transition & animation: transition-shadow

--- pages/playground/transitionsAndAnimation.vue
+++ pages/playground/transitionsAndAnimation.vue
@@ -10,11 +10,18 @@
             class="size-10 bg-red-400 transition-colors hover:bg-sky-400"
           ></div>
         </div>
-        
+
         <div class="mb-3">
           <h3 class="mb-3 text-red-700">transition-colors</h3>
           <div
-            class="size-10 bg-red-400 hover:opacity-70 transition-opacity"
+            class="size-10 bg-red-400 transition-opacity hover:opacity-70"
+          ></div>
+        </div>
+
+        <div class="mb-3">
+          <h3 class="mb-3 text-red-700">transition-shadow</h3>
+          <div
+            class="size-10 bg-red-400 shadow-md shadow-violet-600 transition-shadow hover:shadow-xl hover:shadow-sky-600"
           ></div>
         </div>
       </template>
