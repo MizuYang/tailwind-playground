f83913df04be4c1af4c335ab53265205a60a482d
mizu <psdk55052004@gmail.com>
2024-12-19 01:48:45 +0800

    新增 interactivity: caret-*

--- pages/playground/interactivity.vue
+++ pages/playground/interactivity.vue
@@ -12,7 +12,20 @@
             class="me-5 size-5 accent-violet-700 hover:accent-pink-700/50"
             checked
           />
-          <input type="radio" class="size-5 accent-violet-700 hover:accent-pink-700/50" checked />
+          <input
+            type="radio"
+            class="size-5 accent-violet-700 hover:accent-pink-700/50"
+            checked
+          />
+        </div>
+
+        <div class="mb-3">
+          <h3 class="mb-3 text-red-700">caret-* 光標顏色</h3>
+
+          <input
+            type="text"
+            class="px-3 py-2 text-4xl bg-gray-300 focus:caret-pink-500 ring-4  ring-sky-700 focus:outline-none"
+          />
         </div>
       </template>
     </SectionContent>
