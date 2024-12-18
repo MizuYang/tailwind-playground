799ac6c91db8745e5adfd3845bb05fa8b173c139
mizu <psdk55052004@gmail.com>
2024-12-19 01:56:36 +0800

    新增 interactivity: resize overflow-auto

--- pages/playground/interactivity.vue
+++ pages/playground/interactivity.vue
@@ -27,6 +27,12 @@
             class="px-3 py-2 text-4xl bg-gray-300 focus:caret-pink-500 ring-4  ring-sky-700 focus:outline-none"
           />
         </div>
+
+        <div class="mb-3">
+          <h3 class="mb-3 text-red-700">resize overflow-auto 使元素可拖曳控制寬高</h3>
+
+          <div class="bg-sky-100 ring-1 ring-sky-300 w-24 h-10 resize overflow-auto" contenteditable></div>
+        </div>
       </template>
     </SectionContent>
   </div>
